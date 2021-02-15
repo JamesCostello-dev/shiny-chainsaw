d59d8e5e00a229ce5a9e0ab135803d459755b2c0
JamesCostello-dev <jcotab@gmail.com>
2021-02-15 03:24:23 -0700

    Initial Commit

---
+++ food-tracker/food_trackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  food_trackerApp.swift
+//  food-tracker
+//
+//  Created by James Costello on 2/15/21.
+//
+
+import SwiftUI
+
+@main
+struct food_trackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
